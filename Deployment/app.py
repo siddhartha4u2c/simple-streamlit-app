20b44ccc3f4af82af3db731ce275cce5958c63a5
siddhartha_borpuzari <siddhartha_borpuzari@rediffmail.com>
2025-08-07 22:41:09 +0530

    Add files via upload

--- Deployment/app.py
+++ Deployment/app.py
@@ -5,4 +5,4 @@ st.title("Simple Greeting App 😊")
 name = st.text_input("Enter your name:")
 
 if name:
-    st.write(f"Hello, **{name}**! 👋 Welcome to the Streamlit app.")p
+    st.write(f"Hello, **{name}**! 👋 Welcome to the Streamlit app.")
